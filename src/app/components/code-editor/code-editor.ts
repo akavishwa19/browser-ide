ea9214a808b5bf9e1e087d1c1bcbdae6e9a2a8da
akavishwa19 <akash19102001@gmail.com>
2026-03-23 21:43:18 +0530

    feat: setup diff editor

--- src/app/components/code-editor/code-editor.ts
+++ src/app/components/code-editor/code-editor.ts
@@ -1,7 +1,9 @@
-import { Component, PLATFORM_ID, inject, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
+// import { PLATFORM_ID, inject, } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
-import { isPlatformBrowser } from '@angular/common';
+// import { isPlatformBrowser } from '@angular/common';
+import { DiffEditorModel } from 'ngx-monaco-editor-v2';
 
 @Component({
   selector: 'app-code-editor',
@@ -11,17 +13,29 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class CodeEditor {
   protected readonly title = signal('ide');
-  readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+  // readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
 
-  updateLanguage(language: string) {
-    this.editorOptions = {
-      ...this.editorOptions,
-      language: language,
-    };
-  }
+  // updateLanguage(language: string) {
+  //   this.editorOptions = {
+  //     ...this.editorOptions,
+  //     language: language,
+  //   };
+  // }
 
-  editorOptions = { theme: 'vs-dark', language: 'typescript' };
-  code = 'function x() {\nconsole.log("Hello world!");\n}';
+  // editorOptions = { theme: 'vs-dark', language: 'typescript' };
+  // code = 'function x() {\nconsole.log("Hello world!");\n}';
 
-  name = 's';
+  options = {
+    theme: 'vs-dark',
+    originalEditable: true,
+  };
+  originalModel: DiffEditorModel = {
+    code: 'heLLo world!',
+    language: 'text/plain',
+  };
+
+  modifiedModel: DiffEditorModel = {
+    code: 'hello orlando!',
+    language: 'text/plain',
+  };
 }
